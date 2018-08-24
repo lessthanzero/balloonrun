ccbbab04746025e1be6fd043d47c41c85cab7969
Alexander Katin <aleksandr.katin@gmail.com>
2018-08-24 12:52:02 +0300

    Try to add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,18 @@ module.exports = {
 	    options: {
 	      plugins: [`gatsby-remark-responsive-iframe`]
 	    }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "124353721-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true
+      },
+    },
   ],
 };
